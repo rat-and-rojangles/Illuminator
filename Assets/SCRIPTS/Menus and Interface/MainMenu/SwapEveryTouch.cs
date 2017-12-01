734aa4342f3c1ea80ea189501544c4f568d15541
Brian Fischer <starfox139@yahoo.com>
2017-11-30 21:12:14 -0500

    audio

---
+++ Assets/SCRIPTS/Menus and Interface/MainMenu/SwapEveryTouch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SwapEveryTouch : MonoBehaviour {
+	void Update () {
+#if UNITY_EDITOR
+		if (Input.GetMouseButtonDown (0)) {
+#else
+		if (Utility.ScreenTappedThisFrame ()) {	
+#endif
+			Game.staticRef.planeManager.Swap ();
+		}
+	}
+}
